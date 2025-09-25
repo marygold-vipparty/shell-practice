d5804ea6448fa75442bb33a944d09516349f61b9
marygold-vipparty <marygoldvipparty@gmail.com>
2025-09-25 19:58:14 +0530

    some message

---
+++ 09-data-types.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# everything in shell is considered as string
+NUMBER1=100
+NUMBER2=200
+NAME=DevOps
+
+SUM=$(($NUMBER1+$NUMBER2+$NAME))
+
+echo "SUM is: ${SUM}"
+
+#Size =4, Max index = 3
+LEADERS=("MODI" "PUTIN" "TRUDO" "TRUMP")
+
+echo "All leaders: ${LEADERS[@]}"
+echo "First Leader: ${LEADERS[0]}"
+echo echo "First Leader: ${LEADERS[3]}"
